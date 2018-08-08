8aafd4ca067019b793d57fd4b9b0e17a5ccbe830
Sandeep Acharya <i.am.sandeep.acharya@gmail.com>
2018-08-08 23:14:49 +0530

    Form validation

--- src/routes/api/v1/startup/index.js
+++ src/routes/api/v1/startup/index.js
@@ -5,6 +5,27 @@ const startup = {
     if (!req.body.name) {
       return res.json({error: 'Startup name is required'})
     }
+    if (!req.body.founderName) {
+      return res.json({error: 'Founder name is required'})
+    }
+    if (!req.body.totalMemberCount) {
+      return res.json({error: 'Total member count is missing'})
+    }
+    if (!req.body.typeOfIncorporation) {
+      return res.json({error: 'Incorporation name is required'})
+    }
+    if (!req.body.raisedFunds) {
+      return res.json({error: 'Raisedfund is required'})
+    }
+    if (!req.body.registeredAddress) {
+      return res.json({error: 'Registered Address name is required'})
+    }
+    if (!req.body.founderEmailAddress) {
+      return res.json({error: 'Founder Email Address is missing'})
+    }
+    if (!req.body.founderContactNumber) {
+      return res.json({error: 'Founder Contact Number name is required'})
+    }
     let startupInfo = new StartupInfo(req.body)
     startupInfo.save(err => {
       if (err) {
